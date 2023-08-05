f4c00bb2924ac48b47888a059fb8a471c9321744
KashMihdi <127855730+KashMihdi@users.noreply.github.com>
2023-08-05 16:13:46 +0300

    initial commit

---
+++ Day 47: HabitTracker/HabitTracker/App/HabitTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  HabitTrackerApp.swift
+//  HabitTracker
+//
+//  Created by Kasharin Mikhail on 05.08.2023.
+//
+
+import SwiftUI
+
+@main
+struct HabitTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            HomeView()
+        }
+    }
+}
