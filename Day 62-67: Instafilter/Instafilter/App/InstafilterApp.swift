cec41a21826628b39e899aaefb8228b27b797233
KashMihdi <127855730+KashMihdi@users.noreply.github.com>
2023-09-23 15:32:26 +0300

    finished

---
+++ Day 62-67: Instafilter/Instafilter/App/InstafilterApp.swift
@@ -0,0 +1,17 @@
+//
+//  InstafilterApp.swift
+//  Instafilter
+//
+//  Created by Kasharin Mikhail on 21.09.2023.
+//
+
+import SwiftUI
+
+@main
+struct InstafilterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
